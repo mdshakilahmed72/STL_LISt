79d6a21bb5aabc199a48105ce18b6fa8573356c2
Md Shakil Ahmed <164375897+mdshakilahmed72@users.noreply.github.com>
2024-07-01 16:40:44 +0600

    Create link_listelement.cpp

---
+++ link_listelement.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    list<int> mylist = {20,40,30,10,50,10,10};
+    //mylist.remove(10);
+   // mylist.sort();
+   // mylist.sort(greater<int>());
+  // mylist.unique();
+ // mylist.reverse();
+ //cout<<mylist.front()<<endl<<mylist.back();
+ 
+    // for(int val:mylist){
+    //     cout<<val<<endl;
+    // }
+
+    return 0;
+}
